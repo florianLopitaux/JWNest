9777f389a362c6babda48f98645a220f0bdd9851
florianLopitaux <florian.lopitaux@gmail.com>
2024-07-01 18:49:11 +0200

    Implemented HTMLNode class

---
+++ src/main/java/fr/jwnest/htmlmaker/HTMLNode.java
@@ -0,0 +1,182 @@
+package fr.jwnest.htmlmaker;
+
+import fr.jwnest.utils.ToStringBuilder;
+
+import java.util.*;
+
+public class HTMLNode {
+
+    // ------------------------------------------------------------------------
+    // FIELDS
+    // ------------------------------------------------------------------------
+
+    private final HTMLTag tag;
+    private final List<HTMLNode> children;
+    private final EnumMap<HTMLAttribute, String> attributes;
+
+    private String name;
+    private String value;
+
+    // ------------------------------------------------------------------------
+    // CONSTRUCTORS
+    // ------------------------------------------------------------------------
+
+    public HTMLNode(HTMLTag tag) {
+        this.tag = tag;
+        this.children = new ArrayList<>();
+        this.attributes = new EnumMap<>(HTMLAttribute.class);
+
+        this.name = UUID.randomUUID().toString();
+    }
+
+    public HTMLNode(HTMLTag tag, String value) {
+        this(tag);
+        this.value = value;
+    }
+
+    public HTMLNode(HTMLTag tag, String value, String nodeName) {
+        this(tag, value);
+
+        if (nodeName != null) {
+            this.name = nodeName;
+        }
+    }
+
+    // ------------------------------------------------------------------------
+    // GETTERS
+    // ------------------------------------------------------------------------
+
+    public HTMLTag getTag() {
+        return this.tag;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+
+    public List<HTMLNode> getChildren() {
+        return Collections.unmodifiableList(this.children);
+    }
+
+    public HTMLNode getChild(String nodeName) {
+        for (final HTMLNode child : this.children) {
+            if (child.name.equals(nodeName)) {
+                return child;
+            }
+        }
+
+        return null;
+    }
+
+    public Map<HTMLAttribute, String> getAttributes() {
+        return new EnumMap<>(this.attributes);
+    }
+
+    // ------------------------------------------------------------------------
+    // SETTERS
+    // ------------------------------------------------------------------------
+
+    public void setName(String nodeName) {
+        this.name = nodeName;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public void addChild(HTMLNode child) {
+        this.children.add(child);
+    }
+
+    public void addChildren(List<HTMLNode> children) {
+        this.children.addAll(children);
+    }
+
+    public boolean removeChild(HTMLNode child) {
+        return this.children.remove(child);
+    }
+
+    public boolean removeChild(String nodeName) {
+        boolean found = false;
+
+        for (final HTMLNode child : this.children) {
+            if (child.name.equals(nodeName)) {
+                this.children.remove(child);
+                found = true;
+            }
+        }
+
+        return found;
+    }
+
+    public void setAttribute(HTMLAttribute attribute, String value) {
+        this.attributes.put(attribute, value);
+    }
+
+    public boolean removeAttribute(HTMLAttribute attribute) {
+        return this.attributes.remove(attribute) != null;
+    }
+
+    // ------------------------------------------------------------------------
+    // PUBLIC METHODS
+    // ------------------------------------------------------------------------
+
+    public boolean hasChild(HTMLNode node) {
+        return this.children.contains(node);
+    }
+
+    public boolean hasChild(String nodeName) {
+        for (final HTMLNode child : this.children) {
+            if (child.name.equals(nodeName)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean hasAttribute(HTMLAttribute attribute) {
+        return this.attributes.containsKey(attribute);
+    }
+
+    public void clearChildren() {
+        this.children.clear();
+    }
+
+    public void clearAttributes() {
+        this.attributes.clear();
+    }
+
+    // ------------------------------------------------------------------------
+    // OVERRIDE METHODS FROM Object CLASS
+    // ------------------------------------------------------------------------
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+
+        if (other == null || this.getClass() != other.getClass()) {
+            return false;
+        }
+
+        final HTMLNode htmlNode = (HTMLNode) other;
+        return this.tag.equals(htmlNode.tag) && name.equals(htmlNode.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tag, name);
+    }
+
+    @Override
+    public String toString() {
+        final ToStringBuilder builder = new ToStringBuilder(HTMLNode.class);
+        return builder.build(this.tag, this.children, this.attributes, this.name, this.value);
+    }
+}
